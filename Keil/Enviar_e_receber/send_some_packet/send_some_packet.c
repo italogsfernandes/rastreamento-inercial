450945d5ca9726ef95862a8901bdcaed0f6dc927
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-09-02 09:35:39 -0300

    Biblitecas prontas para manipular o RF

--- Keil/Enviar_e_receber/send_some_packet/send_some_packet.c
+++ Keil/Enviar_e_receber/send_some_packet/send_some_packet.c
@@ -31,5 +31,59 @@
 **************************************************************/
 
 //Example with some numbers
-uint8_t paket2send[26] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-                    21,22,23,24,25};
+uint8_t paket2send[26] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+
+
+
+/**************************************************/
+void delay_time(unsigned long int atr)
+{
+	atr = 2 * atr;
+	while(atr!=0)
+	{
+    _nop_();
+		atr--;
+	}
+}
+/**************************************************/
+void delay(unsigned int x)
+{
+    unsigned int i,j;
+    i=0;
+    for(i=0;i<x;i++)
+    {
+       j=508;
+       while(j--);
+    }
+}
+/**************************************************/
+
+/***************************************************/
+//          - - >    M A I N    < - -
+/**************************************************/
+void main(void)
+{
+
+	// Set up GPIO - FIXME: O que alterar nessa parte?
+	P0DIR = 0xB7;                 // Output: P0.3 e P0.6
+	P1DIR = 0xFF;                 // Output: P0.0 - P0.2, Input: P0.3 - P0.5	 0xFF
+	P2DIR = 0xFF;
+	P0CON = 0x00;                 // All general I/O
+	P1CON = 0x00;                 // All general I/O
+	P2CON = 0x00;                 // All general I/O
+
+	index_in  = 0;
+	index_out = 0;
+	nDataFIFO = 0;
+	FIFOempty = 1;
+
+	rf_init();
+	EA = 1;  											// Enable global IRQ
+	RF = 1; 											// Radio IRQ enable
+	RX_Mode();										// Enable receive values
+
+	while(1)
+	{
+		delay_time(100000); //ql a unidade?
+	}
+}
