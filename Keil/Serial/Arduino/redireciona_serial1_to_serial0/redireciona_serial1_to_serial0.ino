e459ecae924f0f0af1440f07507d02154722e50b
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-09-24 09:22:50 -0300

    Serial com 38400 baudrate funcionando

---
+++ Keil/Serial/Arduino/redireciona_serial1_to_serial0/redireciona_serial1_to_serial0.ino
@@ -0,0 +1,12 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+  Serial1.begin(38400);
+}
+
+void loop() {
+  while(Serial1.available()){
+    Serial.write(Serial1.read());
+  }
+}
