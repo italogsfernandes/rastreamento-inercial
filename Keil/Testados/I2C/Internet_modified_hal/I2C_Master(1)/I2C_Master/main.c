26b453551a401c62252afbb06a775857b52caab4
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-10-22 17:06:05 -0200

    primeiro exemplo de I2C funcionando

--- Keil/Testados/I2C/Internet_modified_hal/I2C_Master(1)/I2C_Master/main.c
+++ Keil/Testados/I2C/Internet_modified_hal/I2C_Master(1)/I2C_Master/main.c
@@ -8,14 +8,31 @@
 #include "nrf24le1.h"
 #include <hal_w2_isr.h>
 #include "hal_delay.h"
+#include "stdint.h"
 
-uint8_t flag =0;
+//uint8_t flag =0;
 
+
+//  Pushbuttons
+sbit S2  = P1^4;    // 1/0 = no/press
+
+
+void italo_delay_ms(unsigned int x)
+{
+    unsigned int i,j;
+    i=0;
+    for(i=0;i<x;i++)
+    {
+       j=508;
+           ;
+       while(j--);
+    }
+}
 void delay_timer0(void);
 
 void main()
 {
-	uint8_t i;
+	//uint8_t i;
 	xdata uint8_t data_string[100];
 	//Indicate startup process by flashing the LED1 
 
@@ -25,34 +42,41 @@ void main()
 	P2DIR = 0xFF;
 	P3DIR = 0xFF;
 //*************************** Init I2C
-  hal_w2_configure_master(HAL_W2_400KHZ);
- 	data_string[0]=0;
+  	hal_w2_configure_master(HAL_W2_100KHZ);
+ 	data_string[0]=0xA6;
 	EA= 1;
-	i=0;
+	//i=0;
 	
 	while(1)	
 	{
 	
+		
 		//store data_string[0] for verification.
-		flag=data_string[0];
+		//flag=data_string[0];
 		//Sending 100 indentical bytes to slave 
 		//P0=0xFF;
-		hal_w2_write(0x07, data_string, 100);
+		if(!S2){
+			hal_w2_write(0x07, data_string, 1);
+			italo_delay_ms(100);
+			while(!S2); //espera soltar o botao
+  			italo_delay_ms(100);
+		}
 		//Reading 100 byte from slave (after increased value by 1 on the slave)
 		//P0=0xAA;
-        hal_w2_read(0x07,data_string,100);
+        //hal_w2_read(0x07,data_string,100);
 		
-		P0=data_string[0]; //Output the byte (optional)
+		//P0=data_string[0]; //Output the byte (optional)
 	
 		//Verification
-		flag++;
-		for (i=0;i<99; i++) 
-		if (data_string[i]!=flag)//not matched
-		{
-			P0=0xAA;while(1); 
-		}
+		//flag++;
+		//for (i=0;i<99; i++) 
+		//if (data_string[i]!=flag)//not matched
+		//{
+		//	P0=0xAA;while(1); 
+		//}
 	}
 }
+
 
 
 
