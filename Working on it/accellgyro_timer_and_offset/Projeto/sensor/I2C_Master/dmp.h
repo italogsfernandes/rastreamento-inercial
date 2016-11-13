d1653b30db9e961286e4a6b00849fdcf09b53b75
Italo Fernandes <italogsfernandes@gmail.com>
2016-11-13 15:00:45 -0200

    usando biblioteca de registradores

--- Working on it/accellgyro_timer_and_offset/Projeto/sensor/I2C_Master/dmp.h
+++ Working on it/accellgyro_timer_and_offset/Projeto/sensor/I2C_Master/dmp.h
@@ -7,20 +7,21 @@ mesma biblioteca. */
 #include <hal_w2_isr.h>
 #include "stdint.h"
 #include "stdbool.h"
-#define	MPU_endereco	0x68
+#include "mpu6050_reg.h"
+#define	MPU_endereco MPU6050_DEFAULT_ADDRESS
 
 uint8_t xdata buffer[14]; //usado em testConnection e getIntStatus
 
 
 void mpu_initialize(void){
-  i2c_mpu_writeBits(MPU_endereco, 0x6B, 2, 3, 0x01);//setClockSource(MPU6050_CLOCK_PLL_XGYRO);
-  i2c_mpu_writeBits(MPU_endereco, 0x1B, 4, 2, 0x00);//setFullScaleGyroRange(MPU6050_GYRO_FS_250);
-  i2c_mpu_writeBits(MPU_endereco, 0x1C, 4, 2, 0x00);//setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
-  i2c_mpu_writeBit(MPU_endereco, 0x6B, 6, false); //setSleepEnabled(false);
+  i2c_mpu_writeBits(MPU_endereco, MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_CLKSEL_BIT, MPU6050_PWR1_CLKSEL_LENGTH, MPU6050_CLOCK_PLL_XGYRO);//setClockSource(MPU6050_CLOCK_PLL_XGYRO);
+  i2c_mpu_writeBits(MPU_endereco, MPU6050_RA_GYRO_CONFIG, MPU6050_GCONFIG_FS_SEL_BIT, MPU6050_GCONFIG_FS_SEL_LENGTH, MPU6050_GYRO_FS_250);//setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  i2c_mpu_writeBits(MPU_endereco, MPU6050_RA_ACCEL_CONFIG, MPU6050_ACONFIG_AFS_SEL_BIT, MPU6050_ACONFIG_AFS_SEL_LENGTH, MPU6050_ACCEL_FS_2);//setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  i2c_mpu_writeBit(MPU_endereco, MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_SLEEP_BIT, false); //setSleepEnabled(false);
 }
 
 bool mpu_testConnection(void){
-    i2c_mpu_readBits(MPU_endereco, 0x75, 6, 6, buffer);
+    i2c_mpu_readBits(MPU_endereco,MPU6050_RA_WHO_AM_I, MPU6050_WHO_AM_I_BIT, MPU6050_WHO_AM_I_LENGTH, buffer);
     return buffer[0] == 0x34;
 }
 
@@ -41,22 +42,22 @@ void getMotion6_packet(uint8_t *packet6){
 }
 
 void setXAccelOffset(int16_t offset) {
-    i2c_mpu_writeWord(MPU_endereco,  0x06, offset);
+    i2c_mpu_writeWord(MPU_endereco,  MPU6050_RA_XA_OFFS_H, offset);
 }
 void setYAccelOffset(int16_t offset) {
-    i2c_mpu_writeWord(MPU_endereco, 0x08, offset);
+    i2c_mpu_writeWord(MPU_endereco, MPU6050_RA_YA_OFFS_H, offset);
 }
 void setZAccelOffset(int16_t offset) {
-    i2c_mpu_writeWord(MPU_endereco, 0x0A, offset);
+    i2c_mpu_writeWord(MPU_endereco, MPU6050_RA_ZA_OFFS_H, offset);
 }
 void setXGyroOffset(int16_t offset) {
-    i2c_mpu_writeWord(MPU_endereco, 0x13, offset);
+    i2c_mpu_writeWord(MPU_endereco, MPU6050_RA_XG_OFFS_USRH, offset);
 }
 void setYGyroOffset(int16_t offset) {
-    i2c_mpu_writeWord(MPU_endereco, 0x15, offset);
+    i2c_mpu_writeWord(MPU_endereco, MPU6050_RA_YG_OFFS_USRH, offset);
 }
 void setZGyroOffset(int16_t offset) {
-    i2c_mpu_writeWord(MPU_endereco, 0x17, offset);
+    i2c_mpu_writeWord(MPU_endereco, MPU6050_RA_ZG_OFFS_USRH, offset);
 }
 
 #endif
