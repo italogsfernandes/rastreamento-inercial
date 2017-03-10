40b6e39619b92f0ed3babcd28ff1a996be4473c6
Italo Fernandes <italogsfernandes@gmail.com>
2017-03-10 17:41:48 -0300

    organizando funcoes

--- Host e Sensor - Andrei/Host-Module/host_code/host.c
+++ Host e Sensor - Andrei/Host-Module/host_code/host.c
@@ -9,162 +9,113 @@
 #define Aquire_Freq 100
 
 //Subenderecos dos sensores possiveis de existir na rede
-uint8_t body_sensors[16] = {
-  0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
-  0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F};
-  //Cada bit representa se um sensor esta ativo na rede ou não
-  uint16_t active_sensors = 0x00;//0000 0000 0000 0000
+uint8_t body_sensors[16] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+//Cada bit representa se um sensor esta ativo na rede ou não
+uint16_t active_sensors = 0x00;//0000 0000 0000 0000
 
-  //////////////////////
-  //Functions in Host //
-  //////////////////////
+//////////////////////
+//Functions in Host //
+//////////////////////
 
-  /**
-  * Envia um dado comando cada sensor listado como ativo.
-  * Utilize request_ack_from_sensors para listar sensores.
-  * @param cmd2send comando que se deseja enviar
-  */
-  void send_cmd_to_active_sensors(uint8_t cmd2send);
+/**
+ * Trata os comandos recebidos por UART,
+ * sendo esses definidos na library  'pacotes_inerciais.h'
+ */
+void uart_communication_handler();
 
-  /**
-  * Requisita que todos os sensores possiveis retornem ack,
-  * com isso é possivel listar os sensores ativos (active_sensors)
-  */
-  //void request_ack_from_sensors();
+/**
+ * Trata os pacotes seriais recebidos. Os redireciona para a interface serial.
+ */
+void rf_communication_handler();
 
-  /**
-  * Envia para os sensores ativos o comando para setar o tipo de pacote
-  * @param pkt_type tipo de pacote de acordo com library pacotes inerciais
-  */
-  //void set_packet_type(uint8_t pkt_type);
+/**
+ * Aguarda o recebimento de um pacote inercial vindo de um dado sensor.
+ * @param sensor Sensor para aguardar sua resposta
+ */
+void wait_rf_response(uint8_t sensor);
 
-  void send_cmd_to_all_addrs(uint8_t cmd2send);
-  //TODO: organizar
-  void send_cmd_to_all_addrs_with_arg(uint8_t cmd2send,uint8_t agr2send);
+/**
+* Envia um dado comando cada sensor listado como ativo.
+* Utiliza polling, ou seja aguardará a resposta do sensor.
+* Utilize request_ack_from_sensors para listar sensores.
+* @param cmd2send comando que se deseja enviar
+*/
+void send_cmd_to_active_sensors(uint8_t cmd2send);
 
-  ///////////////////
-  //Implementation //
-  ///////////////////
+/**
+* Requisita que todos os sensores possiveis retornem ack,
+* com isso é possivel listar os sensores ativos (active_sensors)
+*/
+//void request_ack_from_sensors();
 
-  /**
-  * Seta os pinos do nrf como saidas e entradas de acordo com as funcoes desejadas
-  */
-  void iniciarIO(void){
-    P0DIR = 0x00;   // Tudo output
-    P1DIR = 0x00;   // Tudo output
-    P0CON = 0x00; P1CON = 0x00; //Reseting PxCON registers
-    P0DIR &= ~(1<<3);//P03 = Uart tx = output
-    P0DIR |= 1<<4;//P04 = UART Rx = input
-  }
+/**
+* Envia para os sensores ativos o comando para setar o tipo de pacote
+* @param pkt_type tipo de pacote de acordo com library pacotes inerciais
+*/
+//void set_packet_type(uint8_t pkt_type);
 
-  void setup(){
-    iniciarIO();
-    setup_T0_freq(Aquire_Freq,1);//Time em 100.00250006250157Hz
-    hal_uart_init(UART_BAUD_9K6);
-    rf_init(ADDR_HOST,ADDR_HOST,10,RF_DATA_RATE_2Mbps,RF_TX_POWER_0dBm);
-    P06 = 1;delay_ms(500);	P06 = 0;delay_ms(500);
-    P06 = 1;delay_ms(500);	P06 = 0;delay_ms(500);
-    hal_uart_putstring("Dispositivo Host Iniciado\n");
-  }
+void send_cmd_to_all_addrs(uint8_t cmd2send);
+//TODO: organizar
+void send_cmd_to_all_addrs_with_arg(uint8_t cmd2send,uint8_t agr2send);
 
-  void main(){
-    setup();
-    while(1){ //Loop
+///////////////////
+//Implementation //
+///////////////////
 
-      /////////////////////
-      //Comunicao Serial //
-      /////////////////////
-      if(hal_uart_chars_available()){
-        uart_communication_handler();
-      } /*END IF UART AVAILABLE*/
+/**
+* Seta os pinos do nrf como saidas e entradas de acordo com as funcoes desejadas
+*/
+void iniciarIO(void){
+  P0DIR = 0x00;   // Tudo output
+  P1DIR = 0x00;   // Tudo output
+  P0CON = 0x00; P1CON = 0x00; //Reseting PxCON registers
+  P0DIR &= ~(1<<3);//P03 = Uart tx = output
+  P0DIR |= 1<<4;//P04 = UART Rx = input
+}
 
-      ///////////////////
-      //Comunicacao RF //
-      ///////////////////
-      if(newPayload){
-        rf_communication_handler();
-      }
+void setup(){
+  iniciarIO();
+  setup_T0_freq(Aquire_Freq,1);//Time em 100.00250006250157Hz
+  hal_uart_init(UART_BAUD_9K6);
+  rf_init(ADDR_HOST,ADDR_HOST,10,RF_DATA_RATE_2Mbps,RF_TX_POWER_0dBm);
+  P06 = 1;delay_ms(500);	P06 = 0;delay_ms(500);
+  P06 = 1;delay_ms(500);	P06 = 0;delay_ms(500);
+  hal_uart_putstring("Dispositivo Host Iniciado\n");
+}
 
-      //////////
-      //TIMER //
-      //////////
-      if(timer_elapsed){
-        send_rf_command(CMD_READ,BROADCAST_ADDR);
-        timer_elapsed = 0;
-        P06 = !P06;
-      }
-    } /*END INFINITE LOOP*/
-  } /*END MAIN FUNCTION*/
+void main(){
+  setup();
+  while(1){ //Loop
 
-  /////////////////////
-  //FUNCIONS in Host //
-  /////////////////////
+    /////////////////////
+    //Comunicao Serial //
+    /////////////////////
+    if(hal_uart_chars_available()){
+      uart_communication_handler();
+    } /*END IF UART AVAILABLE*/
 
-  void rf_communication_handler(){
-    //bridge, only redirect the packet to serial
-    redirect_rf_pkt_to_serial(rx_buf, payloadWidth);
-    switch (rx_buf[1]) {
-      case CMD_CONNECTION://sensor alive and responding
-      active_sensors |= 1<<rx_buf[0];
-      break;
-      case CMD_DISCONNECT: //Some sensor has some problem
-      active_sensors &= (~(1<<rx_buf[0]));
-      break;
+    ///////////////////
+    //Comunicacao RF //
+    ///////////////////
+    if(newPayload){
+      rf_communication_handler();
     }
-  }
 
-  //TODO: header da funcao
-  //TODO: make the sensors responsives to all types of commands
-  //TODO: remove BROADCAST_ADDR e enviar um a um
-  void wait_rf_response(uint8_t sensor){
-    while (1) {
-      if(newPayload){ //Se algum sensor respondeu,(nao especifica qual mas algum)
-        if(rx_buf[0] == sensor){
-          rf_communication_handler();
-          sta = 0;
-          newPayload = 0;
-          break;
-        } else { //se outro sensor responder ignorar
-          sta = 0;
-          newPayload = 0;
-        }
-      }
-  }
-}
-
-void send_cmd_to_all_addrs(uint8_t cmd2send){
-  uint8_t i;
-  for (i = 0; i < 16; i++) { //para cada sensor possivel
-    send_rf_command(cmd2send,body_sensors[i]);
-  }
-}
-
-void send_cmd_to_all_addrs_with_arg(uint8_t cmd2send,uint8_t agr2send){
-  uint8_t i;
-  for (i = 0; i < 16; i++) { //para cada sensor possivel
-    send_rf_command_with_arg(cmd2send,agr2send,body_sensors[i]);
-  }
-}
-
-void send_cmd_to_active_sensors(uint8_t cmd2send){
-  uint8_t i;
-  for (i = 0; i < 16; i++) { //para cada sensor possivel
-    if(active_sensors & (1<<i)){//se esta ativo
-      send_rf_command(cmd2send,body_sensors[i]);
-      wait_rf_response(body_sensors[i]);
+    //////////
+    //TIMER //
+    //////////
+    if(timer_elapsed){
+      send_rf_command(CMD_READ,BROADCAST_ADDR);
+      timer_elapsed = 0;
+      P06 = !P06;
     }
-  }
-}
+  } /*END INFINITE LOOP*/
+} /*END MAIN FUNCTION*/
 
-void send_cmd_to_active_sensors_with_arg(uint8_t cmd2send,uint8_t agr2send){
-  uint8_t i;
-  for (i = 0; i < 16; i++) { //para cada sensor possivel
-    if(active_sensors & (1<<i)){//se esta ativo
-      send_rf_command_with_arg(cmd2send,agr2send,body_sensors[i]);
-      wait_rf_response(body_sensors[i]);
-    }
-  }
-}
+/////////////////////
+//FUNCIONS in Host //
+/////////////////////
+
 
 void uart_communication_handler(){
   if(hal_uart_getchar() == UART_START_SIGNAL){ //first byte should be start
@@ -215,4 +166,67 @@ void uart_communication_handler(){
       break;
     } /*END SWITCH*/
   } /*END IF START COMMAND*/
+}
+
+void rf_communication_handler(){
+  //bridge, only redirect the packet to serial
+  redirect_rf_pkt_to_serial(rx_buf, payloadWidth);
+  switch (rx_buf[1]) {
+    case CMD_CONNECTION://sensor alive and responding
+    active_sensors |= 1<<rx_buf[0];
+    break;
+    case CMD_DISCONNECT: //Some sensor has some problem
+    active_sensors &= (~(1<<rx_buf[0]));
+    break;
+  }
+}
+
+void wait_rf_response(uint8_t sensor){
+  while (1) {
+    if(newPayload){ //Se algum sensor respondeu,(nao especifica qual mas algum)
+      if(rx_buf[0] == sensor){
+        rf_communication_handler();
+        sta = 0;
+        newPayload = 0;
+        break;
+      } else { //se outro sensor responder ignorar
+        sta = 0;
+        newPayload = 0;
+      }
+    }
+  }
+}
+
+void send_cmd_to_all_addrs(uint8_t cmd2send){
+  uint8_t i;
+  for (i = 0; i < 16; i++) { //para cada sensor possivel
+    send_rf_command(cmd2send,body_sensors[i]);
+  }
+}
+
+void send_cmd_to_all_addrs_with_arg(uint8_t cmd2send,uint8_t agr2send){
+  uint8_t i;
+  for (i = 0; i < 16; i++) { //para cada sensor possivel
+    send_rf_command_with_arg(cmd2send,agr2send,body_sensors[i]);
+  }
+}
+
+void send_cmd_to_active_sensors(uint8_t cmd2send){
+  uint8_t i;
+  for (i = 0; i < 16; i++) { //para cada sensor possivel
+    if(active_sensors & (1<<i)){//se esta ativo
+      send_rf_command(cmd2send,body_sensors[i]);
+      wait_rf_response(body_sensors[i]);
+    }
+  }
+}
+
+void send_cmd_to_active_sensors_with_arg(uint8_t cmd2send,uint8_t agr2send){
+  uint8_t i;
+  for (i = 0; i < 16; i++) { //para cada sensor possivel
+    if(active_sensors & (1<<i)){//se esta ativo
+      send_rf_command_with_arg(cmd2send,agr2send,body_sensors[i]);
+      wait_rf_response(body_sensors[i]);
+    }
+  }
 }
