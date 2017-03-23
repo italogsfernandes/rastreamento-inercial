041be9462cfb05f498997c60e8a1a6b734f8ffbe
Italo Fernandes <italogsfernandes@gmail.com>
2017-03-23 15:22:12 -0300

    inicio do host

---
+++ Firmware/Arduino/Host-Due-nrf24le01/src/due_host.ino
@@ -0,0 +1,29 @@
+/* ------------------------------------------------------------------------------
+ * FEDERAL UNIVERSITY OF UBERLANDIA
+ * Faculty of Electrical Engineering
+ * Biomedical Engineering Lab
+ * Uberlândia, Brazil
+ * ------------------------------------------------------------------------------
+ * Author: Italo G S Fernandes; Andrei Nakagawa, MSc
+ * contact: italogsfernandes@gmail.com; nakagawa.andrei@gmail.com
+ * URLs: www.biolab.eletrica.ufu.br
+ *       https://github.com/BIOLAB-UFU-BRAZIL
+ *       https://github.com/italogfernandes
+ * ------------------------------------------------------------------------------
+ * Description:
+ * ------------------------------------------------------------------------------
+ * Acknowledgements
+ *  - We would like to thank the open-source community that provided many of the
+ *  source codes necessary for creating this firmware.
+ *  - Jeff Rowberg as the main developer of the I2Cdevlib
+ *  - Luis Ródenas: Our calibration routine is totally based on his code
+ * ------------------------------------------------------------------------------
+ */
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
